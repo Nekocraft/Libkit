6f71e7111ebefb0e900fb28d3991ae65237ff7b8
V10lator <v10lator@myway.de>
2013-03-14 17:36:16 +0100

    Better JavaDoc for the Events.

--- src/main/java/org/libigot/event/entity/EntityAddEvent.java
+++ src/main/java/org/libigot/event/entity/EntityAddEvent.java
@@ -5,11 +5,20 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityEvent;
 
+/**
+ * This Event is called whenever a Entity is added
+ * to a world.
+ */
 public class EntityAddEvent extends EntityEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
     private boolean isCancelled;
 
+    /**
+     * The constructor for the event.
+     * 
+     * @param entity The entity that gets added.
+     */
     public EntityAddEvent(Entity entity) {
         super(entity);
         this.isCancelled = false;
@@ -18,16 +27,16 @@ public class EntityAddEvent extends EntityEvent implements Cancellable {
     /**
      * Sets the cancelled state of the event
      *
-     * @param isCancelled The new cancel state
+     * @param true if the event should be cancelled, false otherwise.
      */
     public void setCancelled(boolean isCancelled) {
         this.isCancelled = isCancelled;
     }
 
     /**
-     * Gets the entity that caused the add event
+     * Gets the entity that caused the add event.
      *
-     * @return The entity that caused the add event
+     * @return The entity that caused the add event.
      */
     @Override
     public Entity getEntity() {
@@ -35,9 +44,9 @@ public class EntityAddEvent extends EntityEvent implements Cancellable {
     }
 
     /**
-     * Gets the cancelled state of the event
+     * Gets the cancelled state of the event.
      *
-     * @return The cancelled state
+     * @return true if the event should be cancelled, false otherwise.
      */
     public boolean isCancelled() {
         return this.isCancelled;
