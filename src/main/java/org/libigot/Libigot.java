b8624e167270c74922e73b109cba02b65b4deaa6
V10lator <v10lator@myway.de>
2013-03-14 22:58:13 +0100

    (Libigot) Add API functions from Surge

---
+++ src/main/java/org/libigot/Libigot.java
@@ -0,0 +1,25 @@
+package org.libigot;
+
+public class Libigot {
+
+    private static LibigotServer server;
+
+    /**
+     * Returns the LibigotServer instance.
+     *
+     * @return The LibigotServer.
+     */
+    public static LibigotServer getServer() {
+        return Libigot.server;
+    }
+
+    public static void setServer(LibigotServer server) {
+        if (server != null) {
+            if (Libigot.server == null) {
+                Libigot.server = server;
+            } else {
+                Libigot.server.setDebug(server.getDebug());
+            }
+        }
+    }
+}
