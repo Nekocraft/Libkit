786bd34e011265dad6142e3f6d48442b142d6b17
Steven Downer <grinch@outlook.com>
2013-06-04 18:59:44 -0500

    (Libigot) Revert "Add Arrow API" Patch
    This is reverted in favor of our more functional version which was
    existant prior to this.
    
    This reverts commit 5822a7720b138f9d83ca85502fe62649671a9eaf.

--- src/main/java/org/bukkit/entity/Arrow.java
+++ src/main/java/org/bukkit/entity/Arrow.java
@@ -3,22 +3,4 @@ package org.bukkit.entity;
 /**
  * Represents an arrow.
  */
-public interface Arrow extends Projectile
-{
-
-    public class Spigot
-    {
-
-        public double getDamage()
-        {
-            throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        public void setDamage(double damage)
-        {
-            throw new UnsupportedOperationException( "Not supported yet." );
-        }
-    }
-
-    Spigot spigot();
-}
+public interface Arrow extends Projectile {}
